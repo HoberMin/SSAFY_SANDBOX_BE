d5f20a7be00011a3fdc2069221ff042767b8dcdd
jiyeon <sjy6736@naver.com>
2024-08-28 15:36:31 +0900

    init setting

---
+++ src/main/java/com/example/skilltestingbe/SkillTestingBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.skilltestingbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillTestingBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkillTestingBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/skilltestingbe/SkillTestingBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.skilltestingbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillTestingBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
