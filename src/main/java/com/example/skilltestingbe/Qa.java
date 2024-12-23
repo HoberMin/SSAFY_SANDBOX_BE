2372ff73bff5f09f4af8decdd8a9ed210fea590f
JIYOUNG KIM <kandallee38@gmail.com>
2024-12-23 20:54:19 +0900

    feat: Add information
    Add information about table name

--- src/main/java/com/example/skilltestingbe/Qa.java
+++ src/main/java/com/example/skilltestingbe/Qa.java
@@ -2,15 +2,10 @@ package com.example.skilltestingbe;
 
 import java.time.LocalDateTime;
 
+import jakarta.persistence.*;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EntityListeners;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -20,9 +15,10 @@ import lombok.NoArgsConstructor;
 @Entity
 @Getter
 @Builder
+@EntityListeners(AuditingEntityListener.class)
+@Table(name = "qa")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
-@EntityListeners(AuditingEntityListener.class)
 public class Qa {
 
 	@Id
