f88bf9965a196f92dc513b65f8787683f36b7cd0
JIYOUNG KIM <kandallee38@gmail.com>
2024-12-23 23:25:12 +0900

    refactor: minor changes

--- src/main/java/com/example/skilltestingbe/api/crud/presentation/CRUDController.java
+++ src/main/java/com/example/skilltestingbe/api/crud/presentation/CRUDController.java
@@ -8,15 +8,12 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
