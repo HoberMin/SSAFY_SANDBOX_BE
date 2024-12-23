4d4c779d6b70a93dada7d5e9e84b4c00c4ff6034
JIYOUNG KIM <kandallee38@gmail.com>
2024-12-23 22:13:14 +0900

    refactor: Add annotation
    Add Hidden to hide from swagger api docs

--- src/main/java/com/example/skilltestingbe/api/qa/presentation/QaController.java
+++ src/main/java/com/example/skilltestingbe/api/qa/presentation/QaController.java
@@ -2,6 +2,7 @@ package com.example.skilltestingbe.api.qa.presentation;
 
 import com.example.skilltestingbe.api.qa.dto.QaRegisterDto;
 import com.example.skilltestingbe.api.qa.service.QaService;
+import io.swagger.v3.oas.annotations.Hidden;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
 
+@Hidden
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/qa")
