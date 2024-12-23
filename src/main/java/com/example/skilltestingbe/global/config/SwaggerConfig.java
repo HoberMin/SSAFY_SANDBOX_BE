18f1cbab5531b34e5ee783525ae6d5b0618d708b
JIYOUNG KIM <kandallee38@gmail.com>
2024-12-23 22:12:20 +0900

    feat: Add Setting
    Add settings about Swagger

---
+++ src/main/java/com/example/skilltestingbe/global/config/SwaggerConfig.java
@@ -0,0 +1,46 @@
+package com.example.skilltestingbe.global.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.security.SecuritySchemes;
+import io.swagger.v3.oas.annotations.servers.Server;
+import lombok.RequiredArgsConstructor;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@OpenAPIDefinition(
+        info = @Info(title = "SSAFY SANDBOX API SPECIFICATION",
+                description = "Specification for service SSAFY SANDBOX",
+                version = "v1.0.0")
+//        servers = @Server(url = "https://api-server.store", description = "HTTPS server")
+)
+@SecuritySchemes(value = {
+        @SecurityScheme(
+                name = "ACCESS",
+                type = SecuritySchemeType.APIKEY,
+                in = SecuritySchemeIn.HEADER,
+                paramName = "access_token"
+        ),
+        @SecurityScheme(
+                name = "REFRESH",
+                type = SecuritySchemeType.APIKEY,
+                in = SecuritySchemeIn.COOKIE,
+                paramName = "refresh_token"
+        )
+})
+@Configuration
+@RequiredArgsConstructor
+public class SwaggerConfig {
+
+    @Bean
+    public GroupedOpenApi v1OpenApi() {
+        return GroupedOpenApi.builder()
+                .group("SSAFY SANDBOX SPEC V1")
+                .pathsToMatch("/**")
+                .build();
+    }
+}
