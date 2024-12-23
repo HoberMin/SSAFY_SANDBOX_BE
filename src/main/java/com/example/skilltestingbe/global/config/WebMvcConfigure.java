3854e8e858830475bb772ca85cfda5398ad4ab40
JIYOUNG KIM <kandallee38@gmail.com>
2024-12-23 22:12:39 +0900

    refactor: Change variables
    Hidden variables

--- src/main/java/com/example/skilltestingbe/global/config/WebMvcConfigure.java
+++ src/main/java/com/example/skilltestingbe/global/config/WebMvcConfigure.java
@@ -1,14 +1,36 @@
 package com.example.skilltestingbe.global.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class WebMvcConfigure implements WebMvcConfigurer {
+
+	private final String frontUrl;
+	private final String backPattern;
+
+	@Autowired
+	public WebMvcConfigure(@Value("${FRONT.URL}") String frontUrl, @Value("${BACK.PATTERN}") String backPattern) {
+		this.frontUrl = frontUrl;
+		this.backPattern = backPattern;
+	}
 
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**")
-			.allowedOrigins("*")
-			.allowedMethods("GET", "POST", "PUT", "DELETE");
+		registry.addMapping(backPattern)
+				.allowedOrigins(frontUrl)
+				.allowedMethods(
+						HttpMethod.GET.name(),
+						HttpMethod.POST.name(),
+						HttpMethod.PATCH.name(),
+						HttpMethod.DELETE.name(),
+						HttpMethod.OPTIONS.name())
+				.allowCredentials(true)
+				.allowedHeaders("*")
+				.exposedHeaders("*");
 	}
 }
